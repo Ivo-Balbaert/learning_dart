341e7fd1bec18b3e505df29d42540039c53178d6
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-11-26 15:55:59 -0500

    remove tabs and widgets1 from chapter_10

--- chapter_10/widgets1/web/widgets1.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:html';
-import 'package:web_ui/web_ui.dart';
-import 'package:widget/widget.dart';
-
-void main() {
-  // Enable this to use Shadow DOM in the browser.
-  //useShadowDom = true;
-}
